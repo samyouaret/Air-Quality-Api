168622ca1978e9a2f9f1ac3d49e66f5d0045013a
samyouaret <samyouaret13@gmail.com>
2022-12-21 17:21:36 +0100

    Remove cors

--- src/infrastructure/gateways/http/routes.ts
+++ src/infrastructure/gateways/http/routes.ts
@@ -4,10 +4,9 @@ import error500 from "./routes/500";
 import AirQualityRoutes from "../../../domains/air-quality/routes";
 import swaggerRoute from "./routes/swagger";
 import AirQualityTrackerRoutes from "../../../domains/air-quality-tracker/routes";
-import cors from "cors";
+
 // loaders will be called in the defined order
 const routes = [
-  () => cors(),
   AirQualityRoutes,
   AirQualityTrackerRoutes,
   indexRoute,
