2a8da51dee59ef3731c6add60f37024f6a2238e5
samyouaret <samyouaret13@gmail.com>
2022-12-21 04:24:38 +0100

    Add integration test for tracker service

--- src/domains/air-quality-tracker/AirQualityTrackerService.ts
+++ src/domains/air-quality-tracker/AirQualityTrackerService.ts
@@ -1,10 +1,11 @@
 import AirQualityTrackerRepository from "./AirQualityTrackerRepository";
 import { AirQualityApiContract } from "../../infrastructure/airq-api/AirQualityApiContract";
+import { AirQualityTracker } from "@prisma/client";
 
 export class AirQualityTrackerService {
   constructor(private api: AirQualityApiContract,private repository: AirQualityTrackerRepository) {}
 
-  async track(longitude:number, latitude:number):Promise<any> {
+  async track(longitude:number, latitude:number):Promise<AirQualityTracker | undefined> {
     let result = await this.api.getNearestCity(longitude, latitude);
     
     return await this.repository.save(longitude, latitude, result.Result.pollution);
--- src/domains/air-quality-tracker/tests/integration/AirQualityService.spec.ts
+++ src/domains/air-quality-tracker/tests/integration/AirQualityService.spec.ts
@@ -1,5 +1,36 @@
 import { IqairApiFactory } from "../../../../infrastructure/airq-api/IqairApiFactory";
+import { createPrisma } from "../../../../infrastructure/databases/createPrisma";
+import AirQualityTrackerRepository from "../../AirQualityTrackerRepository";
+import AirQualityTrackerService from "../../AirQualityTrackerService";
+
+const prisma = createPrisma();
+const iqairApiFactory = new IqairApiFactory();
+const api = iqairApiFactory.create();
+const repository = new AirQualityTrackerRepository(prisma)
+const service = new AirQualityTrackerService(api,repository)
+
+beforeAll(async () => {
+    await prisma.$connect();
+});
+
+afterEach(async() => {
+    await prisma.airQualityTracker.deleteMany();
+});
+
+afterAll(async () => {
+    await prisma.$disconnect();
+});
 
 test('Integration: Should save tracking record in db', async() => {
-    expect(1).toBe(1);
+    const result = await service.track(48.856613, 2.3522219);
+    expect(result).toBeDefined();
+    const newRecord = await prisma.airQualityTracker.findFirst({
+        where: {
+            latitude: 48.856613,
+            longitude: 2.3522219
+        }
+    });
+    expect(newRecord).toBeDefined();
+    expect(newRecord?.latitude).toBe(48.856613);
+    expect(newRecord?.longitude).toBe(2.3522219);
 });
